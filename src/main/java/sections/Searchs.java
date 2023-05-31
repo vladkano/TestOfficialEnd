869816728eaf0f8a7934cd5275d2100d051c1c07
Vlad <toma@poisondrop.ru>
2023-05-31 17:13:16 +0300

    Добавлена проверка на соответствие элементов в выдаче

--- src/main/java/sections/Searchs.java
+++ src/main/java/sections/Searchs.java
@@ -3,6 +3,9 @@ package sections;
 import base.Base;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
 
 public class Searchs extends Base {
     private static final By ToSearch = By.xpath("//div[@class='header__right-side']/span[@class='icon-with-title header__icon-button _search hidden_mobile-tablet']");
@@ -15,7 +18,6 @@ public class Searchs extends Base {
     public String getSearchText() {
         return driver.findElement(successfulSearchText).getText();
     }
-
 
     public void typeSearch(String search) {
         type(search, InputToSearch);
--- src/test/java/functionalTests/SearchTest.java
+++ src/test/java/functionalTests/SearchTest.java
@@ -7,12 +7,20 @@ import io.qameta.allure.Epic;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import sections.Searchs;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertSame;
+
+import java.util.List;
+import java.util.ArrayList;
 
 
 @Epic("Тесты поиска")
   public class SearchTest extends TestBase {
+    private By searchToDesigner;
+
     @BeforeEach
     public void setUp() {
         mainSetUp();
@@ -29,7 +37,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
     }
 
     /*
-     * Поиск товара по наименованию дизайнера "Moonka"
+     * Поиск товара по наименованию дизайнера "Moonka" + проверяем что на странице все товары дизайнера Moonka и ссылка соответствует
      */
     @Test()
     public void searchForAnItemByDesigner() {
@@ -37,12 +45,14 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
         ClickSearch();
         search.InputToSearchWeb("Moonka" + "\n");
         String url = driver.getCurrentUrl();
+        List<WebElement> Moonka = driver.findElements(By.xpath("//a[text()='Moonka']"));
         Assertions.assertAll(
                 () -> assertEquals(TestConfig.SITE_URL + "search?search=Moonka", url));
+                      assertEquals(48, Moonka.size());
     }
 
     /*
-     * Поиск товара по коду "41997"
+     * Поиск товара по коду "41997" + проверяем что такой товар только один и ссылка соответствует
      */
     @Test()
     public void searchForAnItemByCode() {
@@ -50,12 +60,14 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
         ClickSearch();
         search.InputToSearchWeb("41977" + "\n");
         String url = driver.getCurrentUrl();
+        List<WebElement> dataId = driver.findElements(By.xpath("//div[@data-id]"));
         Assertions.assertAll(
                 () -> assertEquals(TestConfig.SITE_URL + "search?search=41977", url));
+                      assertEquals(1, dataId.size());
     }
 
     /*
-     * Поиск товара по названию "Колье цепь из серебра"
+     * Поиск товара по названию "Колье цепь из серебра" + проверяем что товары соответствуют поиску и ссылка соответствует
      */
     @Test()
     public void searchForAnItemByName() {
@@ -63,12 +75,14 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
         ClickSearch();
         search.InputToSearchWeb("Колье цепь из серебра" + "\n");
         String url = driver.getCurrentUrl();
+        List<WebElement> Колье = driver.findElements(By.xpath("//a[contains(text(), 'цепь')or contains(text(), 'серебра')]"));
         Assertions.assertAll(
                 () -> assertEquals(TestConfig.SITE_URL + "search?search=%D0%9A%D0%BE%D0%BB%D1%8C%D0%B5%20%D1%86%D0%B5%D0%BF%D1%8C%20%D0%B8%D0%B7%20%D1%81%D0%B5%D1%80%D0%B5%D0%B1%D1%80%D0%B0", url));
+                      assertEquals(48, Колье.size());
     }
 
     /*
-     * Поиск товара по части имени "Медальон"
+     * Поиск товара по части имени "Медальон" + проверяем что товары соответствуют поиску и ссылка соответствует
      */
     @Test()
     public void searchForAnItemByPartOfTheName() {
@@ -76,12 +90,14 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
         ClickSearch();
         search.InputToSearchWeb("Медальон" + "\n");
         String url = driver.getCurrentUrl();
+        List<WebElement> Медальон = driver.findElements(By.xpath("//a[contains(text(), 'медальон')or contains(text(), 'Медальон')]"));
         Assertions.assertAll(
                 () -> assertEquals(TestConfig.SITE_URL + "search?search=%D0%9C%D0%B5%D0%B4%D0%B0%D0%BB%D1%8C%D0%BE%D0%BD", url));
+                      assertEquals(46, Медальон.size());
     }
 
     /*
-     * Поиск товара по категории "Серьги"
+     * Поиск товара по категории "Серьги" + проверяем что товары соответствуют поиску и ссылка соответствует
      */
     @Test()
     public void searchForAnItemByProductCategory() {
@@ -89,12 +105,14 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
         ClickSearch();
         search.InputToSearchWeb("Серьги" + "\n");
         String url = driver.getCurrentUrl();
+        List<WebElement> Серьги = driver.findElements(By.xpath("//a[contains(text(), 'серьги')or contains(text(), 'Серьги')]"));
         Assertions.assertAll(
                 () -> assertEquals(TestConfig.SITE_URL + "search?search=%D0%A1%D0%B5%D1%80%D1%8C%D0%B3%D0%B8", url));
+                      assertEquals(48, Серьги.size());
     }
 
     /*
-     * Поиск товара по цвету "Розовый"
+     * Поиск товара по цвету "Розовый" + проверяем что товары соответствуют поиску и ссылка соответствует
      */
     @Test()
     public void searchForAnItemByColor() {
@@ -102,12 +120,14 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
         ClickSearch();
         search.InputToSearchWeb("Розовый" + "\n");
         String url = driver.getCurrentUrl();
+        List<WebElement> Розовый = driver.findElements(By.xpath("//a[contains(text(), 'роз')or contains(text(), 'Роз')]"));
         Assertions.assertAll(
                 () -> assertEquals(TestConfig.SITE_URL + "search?search=%D0%A0%D0%BE%D0%B7%D0%BE%D0%B2%D1%8B%D0%B9", url));
+                      assertEquals(44, Розовый.size());
     }
 
     /*
-     * Поиск товара по вставке "Текстиль"
+     * Поиск товара по вставке "Текстиль" + проверяем что товары соответствуют поиску и ссылка соответствует
      */
     @Test()
     public void searchForAnItemByInsertion() {
@@ -115,25 +135,29 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
         ClickSearch();
         search.InputToSearchWeb("Текстиль" + "\n");
         String url = driver.getCurrentUrl();
+        List<WebElement> Текстиль = driver.findElements(By.xpath("//a[contains(text(), 'Текстиль')or contains(text(), 'текстил')]"));
         Assertions.assertAll(
                 () -> assertEquals(TestConfig.SITE_URL + "search?search=%D0%A2%D0%B5%D0%BA%D1%81%D1%82%D0%B8%D0%BB%D1%8C", url));
+                      assertEquals(9,Текстиль.size());
     }
 
     /*
-     * Поиск товара по покрытию "Черный родий"
+     * Поиск товара по покрытию "Черный родий" + проверяем что товары соответствуют поиску и ссылка соответствует
      */
     @Test()
     public void searchForAnItemByCoverage() {
         driver.get(TestConfig.SITE_URL);
         ClickSearch();
         search.InputToSearchWeb("Черный родий" + "\n");
+        List<WebElement> родий = driver.findElements(By.xpath("//a[contains(text(), 'черным')or contains(text(), 'родием')or contains(text(), 'серебра')]"));
         String url = driver.getCurrentUrl();
         Assertions.assertAll(
                 () -> assertEquals(TestConfig.SITE_URL + "search?search=%D0%A7%D0%B5%D1%80%D0%BD%D1%8B%D0%B9%20%D1%80%D0%BE%D0%B4%D0%B8%D0%B9", url));
+                      assertSame(8, родий.size());
     }
 
     /*
-     * Поиск товара по материалу "Фарфор"
+     * Поиск товара по материалу "Фарфор" + проверяем что товары соответствуют поиску и ссылка соответствует
      */
     @Test()
     public void searchForAnItemByMaterial() {
@@ -141,11 +165,13 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
         ClickSearch();
         search.InputToSearchWeb("Фарфор" + "\n");
         String url = driver.getCurrentUrl();
+        List<WebElement> фарфор = driver.findElements(By.xpath("//a[contains(text(), 'фарфор')]"));
         Assertions.assertAll(
                 () -> assertEquals(TestConfig.SITE_URL + "search?search=%D0%A4%D0%B0%D1%80%D1%84%D0%BE%D1%80",url));
+                      assertEquals(25, фарфор.size());
     }
     /*
-     * Поиск товара по коду,котого нет в наличии "29424" + проверка на соответствующую надпись "по вашему запросу ничего не найдено"
+     * Поиск товара по коду,котого нет в наличии "29424" + проверка на соответствующую надпись "по вашему запросу ничего не найдено" и ссылка соответствует
      */
     @Test()
     public void searchForAnItemByCodeIsNotOutOfStock() {
@@ -154,8 +180,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
         search.InputToSearchWeb("29424" + "\n");
         String successfulSearchText = search.getSearchText();
         String url = driver.getCurrentUrl();
-        assertEquals("по вашему запросу ничего не найдено", successfulSearchText);
         Assertions.assertAll(
                 () -> assertEquals(TestConfig.SITE_URL + "search?search=29424",url));
+                      assertEquals("по вашему запросу ничего не найдено", successfulSearchText);
     }
 }
